73195cbecd1263806a97298a4ec729405877e1c4
Edjan Michiles <edjan.michiles@loft.com.br>
2020-11-21 14:43:24 -0300

    feat listen to a port to heroku not crash

--- producer.js
+++ producer.js
@@ -72,3 +72,10 @@ setInterval(async () => {
         publishToQueue(reportCountries, QUEUE_COUNTRIES);
     }
 }, 3000);
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(process.env.PORT);
